bc171097d651262fb041e8eea65aa1b655f858e9
satya kavya <satyakavya24@gmail.com>
2021-11-16 19:29:54 -0600

    time and space complexity of validate sub sequence

--- validateSubSequence.js
+++ validateSubSequence.js
@@ -1,10 +1,15 @@
+
+
+
+//O(n) time && O(n) O(1) space
 function sequence(array, sequence){
     let seqIdx = 0;
     for(const num of array){
         if(seqIdx === sequence.length) break;
         if(sequence[seqIdx] === num) seqIdx++;
     }
-
+   //if the seqIdx is equal to length of an sequence array. that means all the numbers passed a condition num === sequence[seqIdx]
+   //that means all the numbers are matched with an main array
     return seqIdx === sequence.length;
 
 }
